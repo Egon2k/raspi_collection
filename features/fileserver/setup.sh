b34f1ed38656036024eca998f8d48a346fc7c40f
Rene Trampenau <rtrampenau@gmail.com>
2021-12-24 18:01:39 +0100

    updated script

--- features/fileserver/setup.sh
+++ features/fileserver/setup.sh
@@ -4,3 +4,18 @@ sudo apt-get install samba samba-common-bin
 # move standard config file
 sudo mv /etc/samba/smb.conf /etc/samba/backup_smb.conf
 
+# make directory for data
+sudo mkdir /home/pi/data
+
+# get config from github repo
+sudo curl https://raw.githubusercontent.com/Egon2k/raspi_collection/main/features/fileserver/cfg/smb.conf -o /etc/samba/smb.conf
+
+# restart smbd service
+sudo service smbd restart
+
+echo "waiting 10 seconds for samba to restart..."
+sleep 10
+
+# set/password for pi user
+sudo smbpasswd -a pi
+
