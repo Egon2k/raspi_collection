e5984f72a142bd50274a553f9aea79b0ef862a2e
Rene Trampenau <rtrampenau@gmail.com>
2021-12-24 22:00:05 +0100

    small modifications

--- features/fileserver/setup.sh
+++ features/fileserver/setup.sh
@@ -1,13 +1,15 @@
 echo "Installing samba file server..."
 
+sudo apt update && sudo apt upgrade -y
+
 # install necessary packages
-sudo apt-get install samba samba-common-bin
+sudo apt-get install samba samba-common-bin -y
 
 # move standard config file
 sudo mv /etc/samba/smb.conf /etc/samba/backup_smb.conf
 
 # make directory for data
-sudo mkdir /home/pi/data
+sudo mkdir -p /home/pi/data
 
 # get config from github repo
 sudo curl https://raw.githubusercontent.com/Egon2k/raspi_collection/main/features/fileserver/cfg/smb.conf -o /etc/samba/smb.conf
